8de570b38d97547f6595620b29cb0d468afb95cf
Swaroop Kola <swaroopkola77@gmail.com>
2026-09-20 11:31:11 +0530

    Catch undefined JSX components

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import react from 'eslint-plugin-react'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -12,6 +13,7 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      react.configs.flat.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
@@ -22,7 +24,9 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    settings: { react: { version: 'detect' } },
     rules: {
+      'react/jsx-no-undef': 'error',
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
